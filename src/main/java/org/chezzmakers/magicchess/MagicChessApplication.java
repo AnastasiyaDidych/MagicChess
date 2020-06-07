c1e2f587f0b805ba2030445f5c75441bebfd0ff5
merchant <postgetman@gmail.com>
2020-06-07 20:05:29 +0300

    generated default project structure, added .gitignore file, created stub controller

---
+++ src/main/java/org/chezzmakers/magicchess/MagicChessApplication.java
@@ -0,0 +1,13 @@
+package org.chezzmakers.magicchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicChessApplication.class, args);
+	}
+
+}
