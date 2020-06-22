8f4af4527854b5adf6741167e7e3f07d820a79f0
Alexander Chervinskiy <30389135+PostGetMan@users.noreply.github.com>
2020-06-22 13:03:57 +0300

    generated default project structure, added .gitignore file, created stub controller (#1)

---
+++ src/main/java/org/chezzmakers/magicchess/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.chezzmakers.magicchess.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "It has begun!";
+    }
+}
---
+++ src/test/java/org/chezzmakers/magicchess/MagicChessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chezzmakers.magicchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
